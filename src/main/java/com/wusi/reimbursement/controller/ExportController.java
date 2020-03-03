969b97e9e5b29506c5af787238840f5917720e78
wusi <513936307@qq.com>
2020-03-03 16:50:52 +0800

    更新

--- src/main/java/com/wusi/reimbursement/controller/ExportController.java
+++ src/main/java/com/wusi/reimbursement/controller/ExportController.java
@@ -113,7 +113,7 @@ public class ExportController {
         dto.setObjectList(parser(voList));
        // String key = UUID.randomUUID().toString().replaceAll("-", "");
         String serverPath="home/excel/";
-        String filename ="/"+serverPath+DateUtil.formatDate(new Date(), DateUtil.PATTERN_YYYYMMDDHHMMSS) + ".xlsx";
+        String filename =serverPath+DateUtil.formatDate(new Date(), DateUtil.PATTERN_YYYYMMDDHHMMSS) + ".xlsx";
        /* RedisUtil.set(key, dto, 1000 * 60 * 30L);
         String url = excelDownloadUrl + key;*/
         System.out.println(filename);
